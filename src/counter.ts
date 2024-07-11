1b8688ca6a1682458e56bbf409ba9454ab1c2ee2
jjs9241 <jjs9241@p4ace.com>
2024-07-12 03:56:05 +0900

    add counter to calander.

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,39 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+function getTimeRemaining(endtime: string) {
+  const now = new Date();
+  const endTime = new Date(endtime).getTime();
+  const timeDiff = endTime - now.getTime();
+
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((timeDiff / 1000 / 60) % 60);
+  const seconds = Math.floor((timeDiff / 1000) % 60);
+
+  return {
+      total: timeDiff,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+  };
+}
+
+const dDay = '2024-08-24T13:00:00+09:00';
+
+export function setupCounter(counterElement: Element) {
+
+  let clockInterval: NodeJS.Timer | undefined = undefined;
+
+  function updateClock() {
+        try {
+            const remainingTime = getTimeRemaining(dDay);
+            const timeHtml = `<div class="item"><p class="digit">${remainingTime.days}</p><p class="unit">일</p></div><div class="item"><p class="digit">${remainingTime.hours}</p><p class="unit">시간</p></div><div class="item"><p class="digit">${remainingTime.minutes}</p><p class="unit">분</p></div><div class="item"><p class="digit">${remainingTime.seconds}</p><p class="unit">초</p></div>`
+            counterElement.innerHTML = timeHtml
+        } catch (error) {
+            console.error("An error occurred while updating the clock:", error);
+            if(clockInterval) clearInterval(clockInterval);
+        }
+    }
+
+  clockInterval = setInterval(updateClock, 1000);
+  updateClock();
 }
